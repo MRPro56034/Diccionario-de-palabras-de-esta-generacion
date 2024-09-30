5ed3be98ab93419ff3dd41bc301fdec1f6617043
AaronArteta <aaronarteta14@gmail.com>
2024-09-29 21:31:44 -0500

    Update diccionario.py
    cambio de el bucle while mejorado, el otro no servia

--- diccionario.py
+++ diccionario.py
@@ -1,3 +1,4 @@
+import time
 
 meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
@@ -8,10 +9,23 @@ meme_dict = {
             "CREEPY": "Aterrador, siniestro",
             "XD" : "Expresion de gracia o una cara volteada"
             }
-while True:
-            word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
 
-if word in meme_dict.keys():
-    print("El significado es:",meme_dict[word])
-else:
-    print("Esta palabra no esta")
+while True:
+            word = input("Escribe una palabra que no entiendas ,"SALIR", para terminar el programa o "BUSCAR", para añadir nuevas palabras(¡con mayúsculas!): ").upper() #funcion para hacer que wordd sse haga en mayusculas
+
+            if word == "SALIR":
+                        print("Programa finalizado")
+                        time.sleep(1)
+                        break
+            elif word == "AÑADIR":
+                        new_word = input("Escribe la nueva palabra (¡con mayúsculas!): ").upper()
+                        if new_word in meme_dict:
+                                    print(new_word,"ya esta en el diccionario")
+                        else:
+                                    new_meaning = input(f"Escribe el significado de '{new_word}': ")
+                                    meme_dict[new_word] = new_meaning
+                                    print(new_word,"ha sido añadida con exito")
+            elif word in meme_dict.keys():
+                print("El significado es:",meme_dict[word])
+            else:
+                print("Esta palabra no esta")
